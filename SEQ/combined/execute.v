a6af5ed25d94179a1c0f18b06641817b845f228b
Dikshant <dikshant.073@gmail.com>
2024-02-22 10:43:43 +0530

    --allow-empty-messages

--- SEQ/combined/execute.v
+++ SEQ/combined/execute.v
@@ -50,22 +50,10 @@ module exe(
 					val_e = val_a;
 					case(in_fun)
 						4'd0: cnd = 1;
-						4'd6: cnd = ((~(N^V))&~zroFlag);
 						4'd1: cnd = ((N^V)| zroFlag);
 						4'd2: cnd = (N^V);
-						4'd4: cnd = ~zroFlag;
-						4'd5: cnd = ~(N^V);
-						4'd3: cnd = zroFlag;
-					endcase
-				end
-				4'd7:
-				begin
-					case(in_fun)
-						4'd1: cnd = ((N^V)| zroFlag);
 						4'd3: cnd = zroFlag;
 						4'd4: cnd = ~zroFlag;
-						4'd0: cnd = 1;
-						4'd2: cnd = (N^V);
 						4'd5: cnd = ~(N^V);
 						4'd6: cnd = ((~(N^V))&~zroFlag);
 					endcase
@@ -78,18 +66,30 @@ module exe(
 					A = val_b;
 					B = val_a;
 					case(in_fun)
-						4'b0001: ctrl = 2'b01;
 						4'b0000: ctrl = 2'b00;
+						4'b0001: ctrl = 2'b01;
 						4'b0010: ctrl = 2'b10;
 						default: ctrl = 2'b11;
 					endcase
 					val_e = ans;
 					cndsn_code = cond;
 				end
-				4'd8: val_e = -64'd8+val_b;
-				4'd10: val_e = -64'd8+val_b;
-				4'd9: val_e = 64'd8+val_b;
-				4'd11: val_e = 64'd8+val_b;
+				4'd7:
+				begin
+					case(in_fun)
+						4'd0: cnd = 1;
+						4'd1: cnd = ((N^V)| zroFlag);
+						4'd2: cnd = (N^V);
+						4'd3: cnd = zroFlag;
+						4'd4: cnd = ~zroFlag;
+						4'd5: cnd = ~(N^V);
+						4'd6: cnd = ((~(N^V))&~zroFlag);
+					endcase
+				end
+				4'd8: val_e = -64'd1+val_b;
+				4'd9: val_e = 64'd1+val_b;
+				4'd10: val_e = -64'd1+val_b;
+				4'd11: val_e = 64'd1+val_b;
 			endcase
 		end
 	end
