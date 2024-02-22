c7539f115cf3edf1b71284550d2db803af7119ca
Dikshant <121669947+pingu-73@users.noreply.github.com>
2024-02-22 12:03:38 +0530

    Update execute.v

--- SEQ/combined/execute.v
+++ SEQ/combined/execute.v
@@ -1,8 +1,10 @@
 `include "alu.v"
+
 module exe(
     input [3:0] in_code, in_fun,
     input [63:0] val_a, val_b, val_c,
 	input clock,
+
     output reg [63:0] val_e,
     output reg cnd
 );
@@ -24,14 +26,11 @@ module exe(
         V = 0;
 	end
 
-    always@(*)
+    always@(posedge clock)
     begin 
-		if(clock==1)
-		begin
 		V = cond[0];
         N = cond[1];
         zroFlag = cond[2];
-		end
     end
 
     ALU uut(A, B, ctrl, ans, cond);
@@ -51,41 +50,41 @@ module exe(
 					val_e = val_a;
 					case(in_fun)
 						4'd0: cnd = 1;
-						4'd6: cnd = ((~(N^V))&~zroFlag);
 						4'd1: cnd = ((N^V)| zroFlag);
 						4'd2: cnd = (N^V);
-						4'd4: cnd = ~zroFlag;
-						4'd5: cnd = ~(N^V);
-						4'd3: cnd = zroFlag;
-					endcase
-				end
-				4'd7:
-				begin
-					case(in_fun)
-						4'd1: cnd = ((N^V)| zroFlag);
 						4'd3: cnd = zroFlag;
 						4'd4: cnd = ~zroFlag;
-						4'd0: cnd = 1;
-						4'd2: cnd = (N^V);
 						4'd5: cnd = ~(N^V);
 						4'd6: cnd = ((~(N^V))&~zroFlag);
 					endcase
 				end
 				4'd3: val_e = val_c;
-				4'd4: val_e = val_c;
-				4'd5: val_e = val_c;
+				4'd4: val_e = val_c+val_b;
+				4'd5: val_e = val_c+val_b;
 				4'd6:
 				begin
 					A = val_b;
 					B = val_a;
 					case(in_fun)
-						4'b0001: ctrl = 2'b01;
 						4'b0000: ctrl = 2'b00;
+						4'b0001: ctrl = 2'b01;
 						4'b0010: ctrl = 2'b10;
 						default: ctrl = 2'b11;
 					endcase
 					val_e = ans;
 					cndsn_code = cond;
+				end
+				4'd7:
+				begin
+					case(in_fun)
+						4'd0: cnd = 1;
+						4'd1: cnd = ((N^V)| zroFlag);
+						4'd2: cnd = (N^V);
+						4'd3: cnd = zroFlag;
+						4'd4: cnd = ~zroFlag;
+						4'd5: cnd = ~(N^V);
+						4'd6: cnd = ((~(N^V))&~zroFlag);
+					endcase
 				end
 				4'd8: val_e = -64'd1+val_b;
 				4'd9: val_e = 64'd1+val_b;
